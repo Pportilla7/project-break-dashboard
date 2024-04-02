cdaafc325d80d38023d12d7b5591c152a6f21385
Pablo Portilla Rivero <pportilla@rfe.es>
2024-04-02 18:58:08 +0200

    Se cambia la dinamica del algoritmo de creacion de contraseñas por una mas sencilla y se resuelve el ejercicio. Falta por dar estilo

--- JS/generador_pswrd.js
+++ JS/generador_pswrd.js
@@ -8,37 +8,25 @@ const simbolos="!@#$%^&*()-_=+"
 boton.addEventListener('click',()=>{
     const long_psw=document.getElementById("longitud")
     let longitud=long_psw.value;
-    console.log(longitud)
     generadorPassword(longitud);
 })
 
 function generadorPassword(long){
-    //obligamos a tener por lo menos 2 de cada tipo
-    let cant_mayus=2
-    let cant_minus=2
-    let cant_num=2
-    let cant_simb=2
-    let cant_restante=long-8
-
     const tipo=[mayus,minus,numeros,simbolos]
-
-    const indiceAleatorio = Math.floor(Math.random() * tipo.length);
-    const valorAleatorio = tipo[indiceAleatorio];
     
-    if(valorAleatorio==0){
-        console.log("mayusculas")
-    }else{
-        if(valorAleatorio==1){
-            console.log("minusculas")
-        }
-        else{
-            if(valorAleatorio==2)
-            {
-                console.log("numeros")
-            }
-            else{
-                console.log("simbolos")
-            }
-        }
+    let password=[]
+
+    for (let i=0;i<long;i++)
+    {
+        const indiceAleatorio = Math.floor(Math.random() * tipo.length);
+        const tipoDatoAleatorio = tipo[indiceAleatorio];
+        
+        const indiceDatoAleatorio=Math.floor(Math.random()*tipoDatoAleatorio.length)
+        const datoAleatorio=tipoDatoAleatorio[indiceDatoAleatorio]
+
+        console.log(indiceAleatorio,tipoDatoAleatorio,indiceDatoAleatorio,datoAleatorio)
+        password.push(datoAleatorio)
     }
+    const password_str=password.join('')
+    console.log(password_str,password_str.length)
 }
